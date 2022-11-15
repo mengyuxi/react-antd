2016696fa591a71c1c368f0fd4925f77f2fa4022
unknown <you@example.com>
2022-11-15 21:35:42 +0800

    优化

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,3 @@
-/* import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
-
-
-ReactDOM.render(<App type="index" />, document.getElementById('root')); */
-
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
