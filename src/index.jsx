4c19ddcc1d19ccf5e98805dee6699cd94adb116d
unknown <you@example.com>
2022-11-16 00:08:54 +0800

    feat:添加ErrorBoundary组件

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
-
+import ErrorBoundary from './components/ErrorBoundary'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+        <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
